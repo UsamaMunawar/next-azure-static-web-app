b74bec7f4b8fcb94706244d2d09f2e9419db7146
UsamaMunawar <usama.cheema.buic@gmail.com>
2022-10-20 22:07:54 +0500

    new things

--- todo-app/next.config.js
+++ todo-app/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
