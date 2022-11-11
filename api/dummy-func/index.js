7d1d319a035c0b33e01259719a9d3cb6d68294a3
UsamaMunawar <usama.cheema.buic@gmail.com>
2022-11-11 23:55:44 +0500

    another another try

--- api/dummy-func/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req, _) {
-  console.log({ context, req, _ });
-  context.res = {
-    status: 200,
-    body: { message: 'This is an achievement' },
-  };
-};
