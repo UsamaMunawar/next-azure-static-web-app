99aa204aa36e203f86a26ea5d840a67d1bd9b1df
UsamaMunawar <usama.cheema.buic@gmail.com>
2022-11-13 20:36:11 +0500

    changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   reactStrictMode: true,
   swcMinify: true,
-  trailingSlash: 'auto',
 };
 
 module.exports = nextConfig;
